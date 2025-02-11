641230a9b9fa60280020bb55edc2f04852b35f41
Michael Colenso <github@michaelcolenso.com>
2025-02-10 22:40:04 -0800

    Update website with current images and separate image list

---
+++ docs/images.js
@@ -0,0 +1,22 @@
+const images = [
+    {
+        name: '2015-08-27_02-03-03.jpg',
+        date: '2015-08-27',
+        title: 'August 27, 2015'
+    },
+    {
+        name: '2015-09-01_05-15-25.jpg',
+        date: '2015-09-01',
+        title: 'September 1, 2015'
+    },
+    {
+        name: '2015-09-02_21-04-11.jpg',
+        date: '2015-09-02',
+        title: 'September 2, 2015'
+    },
+    {
+        name: '2015-10-18_00-16-38.jpg',
+        date: '2015-10-18',
+        title: 'October 18, 2015'
+    }
+];
