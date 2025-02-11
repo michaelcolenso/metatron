2dfc2c6ab9a577f88a14fe8312b0b093e9cfe907
Michael Colenso <github@michaelcolenso.com>
2025-02-10 22:00:03 -0800

    Fix image deployment and update workflow

--- docs/script.js
+++ docs/script.js
@@ -3,14 +3,15 @@ const photoGrid = document.getElementById("photo-grid");
 // List of images we know exist
 const images = [
     'gobi-sees-you.jpg',
-    '2012-06-05_22-05-18.jpg'
+    '2012-06-05_22-05-18.jpg',
+    '2011-05-20_16-25-28.jpg'
 ];
 
 // Create image elements
 images.forEach(imageName => {
     const img = document.createElement("img");
     img.src = `images/${imageName}`;
-    img.alt = imageName.replace(/\.[^/.]+$/, ""); // Remove file extension for alt text
+    img.alt = imageName.replace(/\.[^/.]+$/, "").replace(/-/g, " "); // Remove file extension and hyphens for alt text
     img.loading = "lazy";
     photoGrid.appendChild(img);
 });
