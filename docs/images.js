095e51d0733dcb882bb6ad752207525ac8d0ae82
Michael Colenso <github@michaelcolenso.com>
2025-02-10 22:55:08 -0800

    Add new photos from 2021 and 2022

--- docs/images.js
+++ docs/images.js
@@ -1,5 +1,15 @@
 const photoList = [
     {
+        name: '2022-01-04_06-43-19.jpg',
+        date: '2022-01-04',
+        title: 'January 4, 2022'
+    },
+    {
+        name: '2021-03-27_05-26-50.jpg',
+        date: '2021-03-27',
+        title: 'March 27, 2021'
+    },
+    {
         name: '2015-08-27_02-03-03.jpg',
         date: '2015-08-27',
         title: 'August 27, 2015'
