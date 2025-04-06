1c06784f8c7fbfb8b1728b495d5cfc1e5e19400f
GitHub Action <action@github.com>
2025-04-06 00:20:39 +0000

    Update photo gallery
      - Generated responsive image sizes
      - Created WebP versions
      - Updated image metadata
      - Optimized for web delivery

--- docs/images.js
+++ docs/images.js
@@ -1,23 +1,8 @@
 const photoList = [
   {
-    "name": "2022-01-04_06-43-19.jpg",
-    "title": "2022 01 04 06 43 19",
-    "date": "2025-03-30T00:20:17.327Z",
-    "camera": null,
-    "exposure": null,
-    "aperture": null,
-    "iso": null,
-    "hasWebP": true,
-    "sizes": {
-      "thumb": "thumb_2022-01-04_06-43-19.jpg",
-      "medium": "medium_2022-01-04_06-43-19.jpg",
-      "full": "2022-01-04_06-43-19.jpg"
-    }
-  },
-  {
     "name": "2023-03-01_21-03-58.jpg",
     "title": "2023 03 01 21 03 58",
-    "date": "2025-03-30T00:20:17.327Z",
+    "date": "2025-04-06T00:19:55.721Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -30,9 +15,54 @@ const photoList = [
     }
   },
   {
+    "name": "2022-01-04_06-43-19.jpg",
+    "title": "2022 01 04 06 43 19",
+    "date": "2025-04-06T00:19:55.720Z",
+    "camera": null,
+    "exposure": null,
+    "aperture": null,
+    "iso": null,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_2022-01-04_06-43-19.jpg",
+      "medium": "medium_2022-01-04_06-43-19.jpg",
+      "full": "2022-01-04_06-43-19.jpg"
+    }
+  },
+  {
+    "name": "2021-03-27_05-26-50.jpg",
+    "title": "2021 03 27 05 26 50",
+    "date": "2025-04-06T00:19:55.719Z",
+    "camera": null,
+    "exposure": null,
+    "aperture": null,
+    "iso": null,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_2021-03-27_05-26-50.jpg",
+      "medium": "medium_2021-03-27_05-26-50.jpg",
+      "full": "2021-03-27_05-26-50.jpg"
+    }
+  },
+  {
+    "name": "2017-12-04_13-37-38.jpg",
+    "title": "2017 12 04 13 37 38",
+    "date": "2025-04-06T00:19:55.718Z",
+    "camera": null,
+    "exposure": null,
+    "aperture": null,
+    "iso": null,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_2017-12-04_13-37-38.jpg",
+      "medium": "medium_2017-12-04_13-37-38.jpg",
+      "full": "2017-12-04_13-37-38.jpg"
+    }
+  },
+  {
     "name": "2018-08-01_03-54-17.jpg",
     "title": "2018 08 01 03 54 17",
-    "date": "2025-03-30T00:20:17.325Z",
+    "date": "2025-04-06T00:19:55.718Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -47,7 +77,7 @@ const photoList = [
   {
     "name": "2018-10-10_04-36-09.jpg",
     "title": "2018 10 10 04 36 09",
-    "date": "2025-03-30T00:20:17.325Z",
+    "date": "2025-04-06T00:19:55.718Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -60,24 +90,9 @@ const photoList = [
     }
   },
   {
-    "name": "2021-03-27_05-26-50.jpg",
-    "title": "2021 03 27 05 26 50",
-    "date": "2025-03-30T00:20:17.325Z",
-    "camera": null,
-    "exposure": null,
-    "aperture": null,
-    "iso": null,
-    "hasWebP": true,
-    "sizes": {
-      "thumb": "thumb_2021-03-27_05-26-50.jpg",
-      "medium": "medium_2021-03-27_05-26-50.jpg",
-      "full": "2021-03-27_05-26-50.jpg"
-    }
-  },
-  {
     "name": "2017-01-31_02-17-15.jpg",
     "title": "2017 01 31 02 17 15",
-    "date": "2025-03-30T00:20:17.324Z",
+    "date": "2025-04-06T00:19:55.717Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -90,24 +105,24 @@ const photoList = [
     }
   },
   {
-    "name": "2017-12-04_13-37-38.jpg",
-    "title": "2017 12 04 13 37 38",
-    "date": "2025-03-30T00:20:17.324Z",
+    "name": "2016-11-03_16-39-25.jpg",
+    "title": "2016 11 03 16 39 25",
+    "date": "2025-04-06T00:19:55.716Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
     "iso": null,
     "hasWebP": true,
     "sizes": {
-      "thumb": "thumb_2017-12-04_13-37-38.jpg",
-      "medium": "medium_2017-12-04_13-37-38.jpg",
-      "full": "2017-12-04_13-37-38.jpg"
+      "thumb": "thumb_2016-11-03_16-39-25.jpg",
+      "medium": "medium_2016-11-03_16-39-25.jpg",
+      "full": "2016-11-03_16-39-25.jpg"
     }
   },
   {
     "name": "2016-12-04_02-09-16.jpg",
     "title": "2016 12 04 02 09 16",
-    "date": "2025-03-30T00:20:17.323Z",
+    "date": "2025-04-06T00:19:55.716Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -122,7 +137,7 @@ const photoList = [
   {
     "name": "2016-12-05_20-16-41.jpg",
     "title": "2016 12 05 20 16 41",
-    "date": "2025-03-30T00:20:17.323Z",
+    "date": "2025-04-06T00:19:55.716Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -135,9 +150,24 @@ const photoList = [
     }
   },
   {
+    "name": "2015-10-18_00-16-38.jpg",
+    "title": "2015 10 18 00 16 38",
+    "date": "2025-04-06T00:19:55.715Z",
+    "camera": null,
+    "exposure": null,
+    "aperture": null,
+    "iso": null,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_2015-10-18_00-16-38.jpg",
+      "medium": "medium_2015-10-18_00-16-38.jpg",
+      "full": "2015-10-18_00-16-38.jpg"
+    }
+  },
+  {
     "name": "2015-11-15_02-55-15.jpg",
     "title": "2015 11 15 02 55 15",
-    "date": "2025-03-30T00:20:17.322Z",
+    "date": "2025-04-06T00:19:55.715Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -152,7 +182,7 @@ const photoList = [
   {
     "name": "2016-09-13_02-14-27.jpg",
     "title": "2016 09 13 02 14 27",
-    "date": "2025-03-30T00:20:17.322Z",
+    "date": "2025-04-06T00:19:55.715Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -165,24 +195,24 @@ const photoList = [
     }
   },
   {
-    "name": "2016-11-03_16-39-25.jpg",
-    "title": "2016 11 03 16 39 25",
-    "date": "2025-03-30T00:20:17.322Z",
+    "name": "2015-08-31_20-26-27.jpg",
+    "title": "2015 08 31 20 26 27",
+    "date": "2025-04-06T00:19:55.714Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
     "iso": null,
     "hasWebP": true,
     "sizes": {
-      "thumb": "thumb_2016-11-03_16-39-25.jpg",
-      "medium": "medium_2016-11-03_16-39-25.jpg",
-      "full": "2016-11-03_16-39-25.jpg"
+      "thumb": "thumb_2015-08-31_20-26-27.jpg",
+      "medium": "medium_2015-08-31_20-26-27.jpg",
+      "full": "2015-08-31_20-26-27.jpg"
     }
   },
   {
     "name": "2015-09-01_05-15-25.jpg",
     "title": "2015 09 01 05 15 25",
-    "date": "2025-03-30T00:20:17.321Z",
+    "date": "2025-04-06T00:19:55.714Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -197,7 +227,7 @@ const photoList = [
   {
     "name": "2015-09-02_21-04-11.jpg",
     "title": "2015 09 02 21 04 11",
-    "date": "2025-03-30T00:20:17.321Z",
+    "date": "2025-04-06T00:19:55.714Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -210,24 +240,9 @@ const photoList = [
     }
   },
   {
-    "name": "2015-10-18_00-16-38.jpg",
-    "title": "2015 10 18 00 16 38",
-    "date": "2025-03-30T00:20:17.321Z",
-    "camera": null,
-    "exposure": null,
-    "aperture": null,
-    "iso": null,
-    "hasWebP": true,
-    "sizes": {
-      "thumb": "thumb_2015-10-18_00-16-38.jpg",
-      "medium": "medium_2015-10-18_00-16-38.jpg",
-      "full": "2015-10-18_00-16-38.jpg"
-    }
-  },
-  {
     "name": "2015-08-24_04-54-31.jpg",
     "title": "2015 08 24 04 54 31",
-    "date": "2025-03-30T00:20:17.320Z",
+    "date": "2025-04-06T00:19:55.713Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -242,7 +257,7 @@ const photoList = [
   {
     "name": "2015-08-27_02-03-03.jpg",
     "title": "2015 08 27 02 03 03",
-    "date": "2025-03-30T00:20:17.320Z",
+    "date": "2025-04-06T00:19:55.713Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -255,24 +270,24 @@ const photoList = [
     }
   },
   {
-    "name": "2015-08-31_20-26-27.jpg",
-    "title": "2015 08 31 20 26 27",
-    "date": "2025-03-30T00:20:17.320Z",
+    "name": "2011-01-19_15-35-45.jpg",
+    "title": "2011 01 19 15 35 45",
+    "date": "2025-04-06T00:19:55.712Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
     "iso": null,
     "hasWebP": true,
     "sizes": {
-      "thumb": "thumb_2015-08-31_20-26-27.jpg",
-      "medium": "medium_2015-08-31_20-26-27.jpg",
-      "full": "2015-08-31_20-26-27.jpg"
+      "thumb": "thumb_2011-01-19_15-35-45.jpg",
+      "medium": "medium_2011-01-19_15-35-45.jpg",
+      "full": "2011-01-19_15-35-45.jpg"
     }
   },
   {
     "name": "2011-02-06_05-51-23.jpg",
     "title": "2011 02 06 05 51 23",
-    "date": "2025-03-30T00:20:17.319Z",
+    "date": "2025-04-06T00:19:55.712Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -287,7 +302,7 @@ const photoList = [
   {
     "name": "2012-03-06_23-17-28.jpg",
     "title": "2012 03 06 23 17 28",
-    "date": "2025-03-30T00:20:17.319Z",
+    "date": "2025-04-06T00:19:55.712Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -302,7 +317,7 @@ const photoList = [
   {
     "name": "2012-07-19_01-05-46.jpg",
     "title": "2012 07 19 01 05 46",
-    "date": "2025-03-30T00:20:17.319Z",
+    "date": "2025-04-06T00:19:55.712Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -317,7 +332,7 @@ const photoList = [
   {
     "name": "2015-08-16_20-41-00.jpg",
     "title": "2015 08 16 20 41 00",
-    "date": "2025-03-30T00:20:17.319Z",
+    "date": "2025-04-06T00:19:55.712Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -327,21 +342,6 @@ const photoList = [
       "thumb": "thumb_2015-08-16_20-41-00.jpg",
       "medium": "medium_2015-08-16_20-41-00.jpg",
       "full": "2015-08-16_20-41-00.jpg"
-    }
-  },
-  {
-    "name": "2011-01-19_15-35-45.jpg",
-    "title": "2011 01 19 15 35 45",
-    "date": "2025-03-30T00:20:17.318Z",
-    "camera": null,
-    "exposure": null,
-    "aperture": null,
-    "iso": null,
-    "hasWebP": true,
-    "sizes": {
-      "thumb": "thumb_2011-01-19_15-35-45.jpg",
-      "medium": "medium_2011-01-19_15-35-45.jpg",
-      "full": "2011-01-19_15-35-45.jpg"
     }
   }
 ];
