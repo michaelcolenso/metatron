c788864c28dd18dd4d6689701503e50c2490681d
GitHub Action <action@github.com>
2025-10-27 01:34:38 +0000

    Update photo gallery
      - Generated responsive image sizes
      - Created WebP versions
      - Updated image metadata
      - Optimized for web delivery

--- docs/images.js
+++ docs/images.js
@@ -1,23 +1,8 @@
 const photoList = [
   {
-    "name": "2022-01-04_06-43-19.jpg",
-    "title": "2022 01 04 06 43 19",
-    "date": "2025-10-27T00:41:49.247Z",
-    "camera": null,
-    "exposure": null,
-    "aperture": null,
-    "iso": null,
-    "hasWebP": true,
-    "sizes": {
-      "thumb": "thumb_2022-01-04_06-43-19.jpg",
-      "medium": "medium_2022-01-04_06-43-19.jpg",
-      "full": "2022-01-04_06-43-19.jpg"
-    }
-  },
-  {
     "name": "2023-03-01_21-03-58.jpg",
     "title": "2023 03 01 21 03 58",
-    "date": "2025-10-27T00:41:49.247Z",
+    "date": "2025-10-27T01:32:13.644Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -30,9 +15,54 @@ const photoList = [
     }
   },
   {
+    "name": "2022-01-04_06-43-19.jpg",
+    "title": "2022 01 04 06 43 19",
+    "date": "2025-10-27T01:32:13.643Z",
+    "camera": null,
+    "exposure": null,
+    "aperture": null,
+    "iso": null,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_2022-01-04_06-43-19.jpg",
+      "medium": "medium_2022-01-04_06-43-19.jpg",
+      "full": "2022-01-04_06-43-19.jpg"
+    }
+  },
+  {
+    "name": "2021-03-27_05-26-50.jpg",
+    "title": "2021 03 27 05 26 50",
+    "date": "2025-10-27T01:32:13.642Z",
+    "camera": null,
+    "exposure": null,
+    "aperture": null,
+    "iso": null,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_2021-03-27_05-26-50.jpg",
+      "medium": "medium_2021-03-27_05-26-50.jpg",
+      "full": "2021-03-27_05-26-50.jpg"
+    }
+  },
+  {
+    "name": "2017-12-04_13-37-38.jpg",
+    "title": "2017 12 04 13 37 38",
+    "date": "2025-10-27T01:32:13.641Z",
+    "camera": null,
+    "exposure": null,
+    "aperture": null,
+    "iso": null,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_2017-12-04_13-37-38.jpg",
+      "medium": "medium_2017-12-04_13-37-38.jpg",
+      "full": "2017-12-04_13-37-38.jpg"
+    }
+  },
+  {
     "name": "2018-08-01_03-54-17.jpg",
     "title": "2018 08 01 03 54 17",
-    "date": "2025-10-27T00:41:49.245Z",
+    "date": "2025-10-27T01:32:13.641Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -47,7 +77,7 @@ const photoList = [
   {
     "name": "2018-10-10_04-36-09.jpg",
     "title": "2018 10 10 04 36 09",
-    "date": "2025-10-27T00:41:49.245Z",
+    "date": "2025-10-27T01:32:13.641Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -60,69 +90,9 @@ const photoList = [
     }
   },
   {
-    "name": "2021-03-27_05-26-50.jpg",
-    "title": "2021 03 27 05 26 50",
-    "date": "2025-10-27T00:41:49.245Z",
-    "camera": null,
-    "exposure": null,
-    "aperture": null,
-    "iso": null,
-    "hasWebP": true,
-    "sizes": {
-      "thumb": "thumb_2021-03-27_05-26-50.jpg",
-      "medium": "medium_2021-03-27_05-26-50.jpg",
-      "full": "2021-03-27_05-26-50.jpg"
-    }
-  },
-  {
-    "name": "2017-01-31_02-17-15.jpg",
-    "title": "2017 01 31 02 17 15",
-    "date": "2025-10-27T00:41:49.244Z",
-    "camera": null,
-    "exposure": null,
-    "aperture": null,
-    "iso": null,
-    "hasWebP": true,
-    "sizes": {
-      "thumb": "thumb_2017-01-31_02-17-15.jpg",
-      "medium": "medium_2017-01-31_02-17-15.jpg",
-      "full": "2017-01-31_02-17-15.jpg"
-    }
-  },
-  {
-    "name": "2017-12-04_13-37-38.jpg",
-    "title": "2017 12 04 13 37 38",
-    "date": "2025-10-27T00:41:49.244Z",
-    "camera": null,
-    "exposure": null,
-    "aperture": null,
-    "iso": null,
-    "hasWebP": true,
-    "sizes": {
-      "thumb": "thumb_2017-12-04_13-37-38.jpg",
-      "medium": "medium_2017-12-04_13-37-38.jpg",
-      "full": "2017-12-04_13-37-38.jpg"
-    }
-  },
-  {
-    "name": "2016-12-04_02-09-16.jpg",
-    "title": "2016 12 04 02 09 16",
-    "date": "2025-10-27T00:41:49.243Z",
-    "camera": null,
-    "exposure": null,
-    "aperture": null,
-    "iso": null,
-    "hasWebP": true,
-    "sizes": {
-      "thumb": "thumb_2016-12-04_02-09-16.jpg",
-      "medium": "medium_2016-12-04_02-09-16.jpg",
-      "full": "2016-12-04_02-09-16.jpg"
-    }
-  },
-  {
     "name": "2016-12-05_20-16-41.jpg",
     "title": "2016 12 05 20 16 41",
-    "date": "2025-10-27T00:41:49.243Z",
+    "date": "2025-10-27T01:32:13.640Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -135,9 +105,69 @@ const photoList = [
     }
   },
   {
+    "name": "2017-01-31_02-17-15.jpg",
+    "title": "2017 01 31 02 17 15",
+    "date": "2025-10-27T01:32:13.640Z",
+    "camera": null,
+    "exposure": null,
+    "aperture": null,
+    "iso": null,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_2017-01-31_02-17-15.jpg",
+      "medium": "medium_2017-01-31_02-17-15.jpg",
+      "full": "2017-01-31_02-17-15.jpg"
+    }
+  },
+  {
+    "name": "2016-11-03_16-39-25.jpg",
+    "title": "2016 11 03 16 39 25",
+    "date": "2025-10-27T01:32:13.639Z",
+    "camera": null,
+    "exposure": null,
+    "aperture": null,
+    "iso": null,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_2016-11-03_16-39-25.jpg",
+      "medium": "medium_2016-11-03_16-39-25.jpg",
+      "full": "2016-11-03_16-39-25.jpg"
+    }
+  },
+  {
+    "name": "2016-12-04_02-09-16.jpg",
+    "title": "2016 12 04 02 09 16",
+    "date": "2025-10-27T01:32:13.639Z",
+    "camera": null,
+    "exposure": null,
+    "aperture": null,
+    "iso": null,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_2016-12-04_02-09-16.jpg",
+      "medium": "medium_2016-12-04_02-09-16.jpg",
+      "full": "2016-12-04_02-09-16.jpg"
+    }
+  },
+  {
+    "name": "2015-10-18_00-16-38.jpg",
+    "title": "2015 10 18 00 16 38",
+    "date": "2025-10-27T01:32:13.638Z",
+    "camera": null,
+    "exposure": null,
+    "aperture": null,
+    "iso": null,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_2015-10-18_00-16-38.jpg",
+      "medium": "medium_2015-10-18_00-16-38.jpg",
+      "full": "2015-10-18_00-16-38.jpg"
+    }
+  },
+  {
     "name": "2015-11-15_02-55-15.jpg",
     "title": "2015 11 15 02 55 15",
-    "date": "2025-10-27T00:41:49.242Z",
+    "date": "2025-10-27T01:32:13.638Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -152,7 +182,7 @@ const photoList = [
   {
     "name": "2016-09-13_02-14-27.jpg",
     "title": "2016 09 13 02 14 27",
-    "date": "2025-10-27T00:41:49.242Z",
+    "date": "2025-10-27T01:32:13.638Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -165,84 +195,9 @@ const photoList = [
     }
   },
   {
-    "name": "2016-11-03_16-39-25.jpg",
-    "title": "2016 11 03 16 39 25",
-    "date": "2025-10-27T00:41:49.242Z",
-    "camera": null,
-    "exposure": null,
-    "aperture": null,
-    "iso": null,
-    "hasWebP": true,
-    "sizes": {
-      "thumb": "thumb_2016-11-03_16-39-25.jpg",
-      "medium": "medium_2016-11-03_16-39-25.jpg",
-      "full": "2016-11-03_16-39-25.jpg"
-    }
-  },
-  {
-    "name": "2015-09-01_05-15-25.jpg",
-    "title": "2015 09 01 05 15 25",
-    "date": "2025-10-27T00:41:49.241Z",
-    "camera": null,
-    "exposure": null,
-    "aperture": null,
-    "iso": null,
-    "hasWebP": true,
-    "sizes": {
-      "thumb": "thumb_2015-09-01_05-15-25.jpg",
-      "medium": "medium_2015-09-01_05-15-25.jpg",
-      "full": "2015-09-01_05-15-25.jpg"
-    }
-  },
-  {
-    "name": "2015-09-02_21-04-11.jpg",
-    "title": "2015 09 02 21 04 11",
-    "date": "2025-10-27T00:41:49.241Z",
-    "camera": null,
-    "exposure": null,
-    "aperture": null,
-    "iso": null,
-    "hasWebP": true,
-    "sizes": {
-      "thumb": "thumb_2015-09-02_21-04-11.jpg",
-      "medium": "medium_2015-09-02_21-04-11.jpg",
-      "full": "2015-09-02_21-04-11.jpg"
-    }
-  },
-  {
-    "name": "2015-10-18_00-16-38.jpg",
-    "title": "2015 10 18 00 16 38",
-    "date": "2025-10-27T00:41:49.241Z",
-    "camera": null,
-    "exposure": null,
-    "aperture": null,
-    "iso": null,
-    "hasWebP": true,
-    "sizes": {
-      "thumb": "thumb_2015-10-18_00-16-38.jpg",
-      "medium": "medium_2015-10-18_00-16-38.jpg",
-      "full": "2015-10-18_00-16-38.jpg"
-    }
-  },
-  {
-    "name": "2015-08-24_04-54-31.jpg",
-    "title": "2015 08 24 04 54 31",
-    "date": "2025-10-27T00:41:49.240Z",
-    "camera": null,
-    "exposure": null,
-    "aperture": null,
-    "iso": null,
-    "hasWebP": true,
-    "sizes": {
-      "thumb": "thumb_2015-08-24_04-54-31.jpg",
-      "medium": "medium_2015-08-24_04-54-31.jpg",
-      "full": "2015-08-24_04-54-31.jpg"
-    }
-  },
-  {
     "name": "2015-08-27_02-03-03.jpg",
     "title": "2015 08 27 02 03 03",
-    "date": "2025-10-27T00:41:49.240Z",
+    "date": "2025-10-27T01:32:13.637Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -257,7 +212,7 @@ const photoList = [
   {
     "name": "2015-08-31_20-26-27.jpg",
     "title": "2015 08 31 20 26 27",
-    "date": "2025-10-27T00:41:49.240Z",
+    "date": "2025-10-27T01:32:13.637Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -270,39 +225,39 @@ const photoList = [
     }
   },
   {
-    "name": "2012-03-06_23-17-28.jpg",
-    "title": "2012 03 06 23 17 28",
-    "date": "2025-10-27T00:41:49.239Z",
+    "name": "2015-09-01_05-15-25.jpg",
+    "title": "2015 09 01 05 15 25",
+    "date": "2025-10-27T01:32:13.637Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
     "iso": null,
     "hasWebP": true,
     "sizes": {
-      "thumb": "thumb_2012-03-06_23-17-28.jpg",
-      "medium": "medium_2012-03-06_23-17-28.jpg",
-      "full": "2012-03-06_23-17-28.jpg"
+      "thumb": "thumb_2015-09-01_05-15-25.jpg",
+      "medium": "medium_2015-09-01_05-15-25.jpg",
+      "full": "2015-09-01_05-15-25.jpg"
     }
   },
   {
-    "name": "2012-07-19_01-05-46.jpg",
-    "title": "2012 07 19 01 05 46",
-    "date": "2025-10-27T00:41:49.239Z",
+    "name": "2015-09-02_21-04-11.jpg",
+    "title": "2015 09 02 21 04 11",
+    "date": "2025-10-27T01:32:13.637Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
     "iso": null,
     "hasWebP": true,
     "sizes": {
-      "thumb": "thumb_2012-07-19_01-05-46.jpg",
-      "medium": "medium_2012-07-19_01-05-46.jpg",
-      "full": "2012-07-19_01-05-46.jpg"
+      "thumb": "thumb_2015-09-02_21-04-11.jpg",
+      "medium": "medium_2015-09-02_21-04-11.jpg",
+      "full": "2015-09-02_21-04-11.jpg"
     }
   },
   {
     "name": "2015-08-16_20-41-00.jpg",
     "title": "2015 08 16 20 41 00",
-    "date": "2025-10-27T00:41:49.239Z",
+    "date": "2025-10-27T01:32:13.636Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -315,9 +270,24 @@ const photoList = [
     }
   },
   {
+    "name": "2015-08-24_04-54-31.jpg",
+    "title": "2015 08 24 04 54 31",
+    "date": "2025-10-27T01:32:13.636Z",
+    "camera": null,
+    "exposure": null,
+    "aperture": null,
+    "iso": null,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_2015-08-24_04-54-31.jpg",
+      "medium": "medium_2015-08-24_04-54-31.jpg",
+      "full": "2015-08-24_04-54-31.jpg"
+    }
+  },
+  {
     "name": "2011-01-19_15-35-45.jpg",
     "title": "2011 01 19 15 35 45",
-    "date": "2025-10-27T00:41:49.238Z",
+    "date": "2025-10-27T01:32:13.635Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -332,7 +302,7 @@ const photoList = [
   {
     "name": "2011-02-06_05-51-23.jpg",
     "title": "2011 02 06 05 51 23",
-    "date": "2025-10-27T00:41:49.238Z",
+    "date": "2025-10-27T01:32:13.635Z",
     "camera": null,
     "exposure": null,
     "aperture": null,
@@ -342,6 +312,201 @@ const photoList = [
       "thumb": "thumb_2011-02-06_05-51-23.jpg",
       "medium": "medium_2011-02-06_05-51-23.jpg",
       "full": "2011-02-06_05-51-23.jpg"
+    }
+  },
+  {
+    "name": "2012-03-06_23-17-28.jpg",
+    "title": "2012 03 06 23 17 28",
+    "date": "2025-10-27T01:32:13.635Z",
+    "camera": null,
+    "exposure": null,
+    "aperture": null,
+    "iso": null,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_2012-03-06_23-17-28.jpg",
+      "medium": "medium_2012-03-06_23-17-28.jpg",
+      "full": "2012-03-06_23-17-28.jpg"
+    }
+  },
+  {
+    "name": "2012-07-19_01-05-46.jpg",
+    "title": "2012 07 19 01 05 46",
+    "date": "2025-10-27T01:32:13.635Z",
+    "camera": null,
+    "exposure": null,
+    "aperture": null,
+    "iso": null,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_2012-07-19_01-05-46.jpg",
+      "medium": "medium_2012-07-19_01-05-46.jpg",
+      "full": "2012-07-19_01-05-46.jpg"
+    }
+  },
+  {
+    "name": "IMG_0639_Original.jpeg",
+    "title": "IMG 0639 Original",
+    "date": "2023-03-11T16:38:33.000Z",
+    "camera": "Canon EOS Rebel T7",
+    "exposure": "1/1250",
+    "aperture": 1.4,
+    "iso": 100,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_IMG_0639_Original.jpeg",
+      "medium": "medium_IMG_0639_Original.jpeg",
+      "full": "IMG_0639_Original.jpeg"
+    }
+  },
+  {
+    "name": "IMG_2456_Original.jpeg",
+    "title": "IMG 2456 Original",
+    "date": "2022-02-21T19:00:10.000Z",
+    "camera": "Canon EOS Rebel T7",
+    "exposure": "1/60",
+    "aperture": 1.6,
+    "iso": 1250,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_IMG_2456_Original.jpeg",
+      "medium": "medium_IMG_2456_Original.jpeg",
+      "full": "IMG_2456_Original.jpeg"
+    }
+  },
+  {
+    "name": "IMG_1149 Copy_Original.jpeg",
+    "title": "IMG 1149 Copy Original",
+    "date": "2021-11-21T15:37:27.000Z",
+    "camera": "Canon EOS Rebel T7",
+    "exposure": "1/400",
+    "aperture": 7.1,
+    "iso": 100,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_IMG_1149 Copy_Original.jpeg",
+      "medium": "medium_IMG_1149 Copy_Original.jpeg",
+      "full": "IMG_1149 Copy_Original.jpeg"
+    }
+  },
+  {
+    "name": "IMG_7578_Original.jpeg",
+    "title": "IMG 7578 Original",
+    "date": "2021-03-03T16:35:17.000Z",
+    "camera": "iPhone 12 Pro",
+    "exposure": "1/2132",
+    "aperture": 1.6,
+    "iso": 32,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_IMG_7578_Original.jpeg",
+      "medium": "medium_IMG_7578_Original.jpeg",
+      "full": "IMG_7578_Original.jpeg"
+    }
+  },
+  {
+    "name": "IMG_5322-EDIT_Original.jpeg",
+    "title": "IMG 5322 EDIT Original",
+    "date": "2009-01-09T22:07:44.000Z",
+    "camera": "Canon EOS DIGITAL REBEL XT",
+    "exposure": "1/640",
+    "aperture": 16,
+    "iso": 800,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_IMG_5322-EDIT_Original.jpeg",
+      "medium": "medium_IMG_5322-EDIT_Original.jpeg",
+      "full": "IMG_5322-EDIT_Original.jpeg"
+    }
+  },
+  {
+    "name": "IMG_5287-EDIT_Original.jpeg",
+    "title": "IMG 5287 EDIT Original",
+    "date": "2009-01-09T22:01:15.000Z",
+    "camera": "Canon EOS DIGITAL REBEL XT",
+    "exposure": "1/250",
+    "aperture": 10,
+    "iso": 800,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_IMG_5287-EDIT_Original.jpeg",
+      "medium": "medium_IMG_5287-EDIT_Original.jpeg",
+      "full": "IMG_5287-EDIT_Original.jpeg"
+    }
+  },
+  {
+    "name": "IMG_5329 Copy_Original.jpeg",
+    "title": "IMG 5329 Copy Original",
+    "date": "2009-01-09T14:08:24.000Z",
+    "camera": "Canon EOS DIGITAL REBEL XT",
+    "exposure": "1/500",
+    "aperture": 14,
+    "iso": 800,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_IMG_5329 Copy_Original.jpeg",
+      "medium": "medium_IMG_5329 Copy_Original.jpeg",
+      "full": "IMG_5329 Copy_Original.jpeg"
+    }
+  },
+  {
+    "name": "IMG_5316 Copy Copy_Original.jpeg",
+    "title": "IMG 5316 Copy Copy Original",
+    "date": "2009-01-09T14:07:04.000Z",
+    "camera": "Canon EOS DIGITAL REBEL XT",
+    "exposure": "1/500",
+    "aperture": 14,
+    "iso": 800,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_IMG_5316 Copy Copy_Original.jpeg",
+      "medium": "medium_IMG_5316 Copy Copy_Original.jpeg",
+      "full": "IMG_5316 Copy Copy_Original.jpeg"
+    }
+  },
+  {
+    "name": "IMG_5310_Original.jpeg",
+    "title": "IMG 5310 Original",
+    "date": "2009-01-09T14:06:21.000Z",
+    "camera": "Canon EOS DIGITAL REBEL XT",
+    "exposure": "1/400",
+    "aperture": 13,
+    "iso": 800,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_IMG_5310_Original.jpeg",
+      "medium": "medium_IMG_5310_Original.jpeg",
+      "full": "IMG_5310_Original.jpeg"
+    }
+  },
+  {
+    "name": "IMG_5308_Original.jpeg",
+    "title": "IMG 5308 Original",
+    "date": "2009-01-09T14:06:11.000Z",
+    "camera": "Canon EOS DIGITAL REBEL XT",
+    "exposure": "1/400",
+    "aperture": 16,
+    "iso": 800,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_IMG_5308_Original.jpeg",
+      "medium": "medium_IMG_5308_Original.jpeg",
+      "full": "IMG_5308_Original.jpeg"
+    }
+  },
+  {
+    "name": "OCT 08 088 Copy Copy.jpeg",
+    "title": "OCT 08 088 Copy Copy",
+    "date": "2008-10-11T07:28:26.000Z",
+    "camera": "Canon EOS DIGITAL REBEL XT",
+    "exposure": "1/320",
+    "aperture": 11,
+    "iso": 400,
+    "hasWebP": true,
+    "sizes": {
+      "thumb": "thumb_OCT 08 088 Copy Copy.jpeg",
+      "medium": "medium_OCT 08 088 Copy Copy.jpeg",
+      "full": "OCT 08 088 Copy Copy.jpeg"
     }
   }
 ];
